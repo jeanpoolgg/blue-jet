e392efdfbd15b4122627c2f46e04127c09e45e2f
Jean Pool Gutierrez <jean-gutierrez-117@hotmail.com>
2023-10-29 01:44:40 -0500

    Agregar 2 naves en diferentes ejes x con movimiento independiente

--- main.cpp
+++ main.cpp
@@ -2,6 +2,116 @@
 
 using namespace miniwin;
 
+    void enemigo1 (int x, int y){
+
+     color_rgb(255, 225, 223); // GRIS
+     rectangulo_lleno(76+x, 32+y, 85+x, 38+y);
+     rectangulo_lleno(79+x, 29+y, 88+x, 35+y);
+
+     color_rgb(169, 195, 220); // GRIS CELESTE
+     rectangulo_lleno(73+x, 35+y, 97+x, 41+y);
+     rectangulo_lleno(88+x, 29+y, 94+x, 35+y);
+
+     color_rgb(119, 170, 161); // VERDE CLARO
+     rectangulo_lleno(64+x, 38+y, 76+x, 47+y);
+     rectangulo_lleno(76+x, 44+y, 97+x, 47+y);
+     rectangulo_lleno(94+x, 38+y, 106+x, 46+y);
+
+     color_rgb(32, 53, 74); // VERDE OSCURO
+     rectangulo_lleno(61+x, 38+y, 64+x, 44+y);
+     rectangulo_lleno(64+x, 35+y, 67+x, 38+y); rectangulo_lleno(64+x, 44+y, 67+x, 47+y);
+     rectangulo_lleno(67+x, 35+y, 73+x, 38+y); rectangulo_lleno(67+x, 47+y, 73+x, 50+y);
+     rectangulo_lleno(70+x, 32+y, 73+x, 35+y);
+     rectangulo_lleno(73+x, 32+y, 76+x, 35+y); rectangulo_lleno(73+x, 38+y, 76+x, 41+y); rectangulo_lleno(73+x, 50+y, 96+x, 53+y);
+     rectangulo_lleno(76+x, 29+y, 79+x, 32+y); rectangulo_lleno(76+x, 41+y, 93+x, 44+y);
+     rectangulo_lleno(79+x, 26+y, 91+x, 29+y);
+     rectangulo_lleno(91+x, 29+y, 94+x, 32+y);
+     rectangulo_lleno(94+x, 32+y, 100+x, 35+y); rectangulo_lleno(94+x, 38+y, 97+x, 41+y);
+     rectangulo_lleno(97+x, 35+y, 106+x, 38+y); rectangulo_lleno(97+x, 47+y, 103+x, 50+y);
+     rectangulo_lleno(103+x, 44+y, 106+x, 47+y);
+     rectangulo_lleno(106+x, 38+y, 109+x, 44+y);
+
+     color_rgb(62, 101, 108); // VERDE
+     rectangulo_lleno(64+x, 42+y, 67+x, 45+y);
+     rectangulo_lleno(67+x, 44+y, 73+x, 47+y);
+     rectangulo_lleno(73+x, 47+y, 97+x, 50+y);
+     rectangulo_lleno(97+x, 44+y, 103+x, 47+y);
+     rectangulo_lleno(103+x, 41+y, 107+x, 44+y);
+
+     color_rgb(249, 193, 74); // AMARILLO
+     rectangulo_lleno(73+x, 41+y, 76+x, 44+y);
+     rectangulo_lleno(94+x, 41+y, 97+x, 44+y);
+
+     color_rgb(255, 255, 255); // BLANCO
+     rectangulo_lleno(79+x, 44+y, 82+x, 47+y);
+     rectangulo_lleno(88+x, 44+y, 91+x, 47+y);
+}
+
+
+
+void enemigo2 (int x, int y){
+
+    color_rgb(62, 101, 108); // VERDE
+    rectangulo_lleno(191+x, 117+y, 236+x, 124+y);
+    rectangulo_lleno(191+x, 118+y, 194+x, 121+y);
+    rectangulo_lleno(230+x, 115+y, 239+x, 121+y);
+
+    color_rgb(255, 225, 223); // GRIS
+    rectangulo_lleno(200+x, 100+y, 206+x, 106+y);
+    rectangulo_lleno(209+x, 97+y, 221+x, 109+y);
+
+    color_rgb(169, 195, 220); // GRIS CELESTE
+    rectangulo_lleno(197+x, 106+y, 233+x, 115+y);
+    rectangulo_lleno(224+x, 100+y, 230+x, 106+y);
+
+    color_rgb(119, 170, 161); // VERDE CLARO
+    rectangulo_lleno(191+x, 112+y, 200+x, 118+y);
+    rectangulo_lleno(194+x, 118+y, 230+x, 121+y);
+    rectangulo_lleno(230+x, 115+y, 233+x, 118+y);
+    rectangulo_lleno(233+x, 112+y, 236+x, 115+y);
+
+    color_rgb(32, 53, 74); // VERDE OSCURO
+    rectangulo_lleno(185+x, 121+y, 188+x, 124+y);
+    rectangulo_lleno(188+x, 115+y, 191+x, 121+y);
+    rectangulo_lleno(191+x, 112+y, 194+x, 115+y); rectangulo_lleno(191+x, 121+y, 194+x, 124+y); rectangulo_lleno(191+x, 127+y, 194+x, 130+y);
+    rectangulo_lleno(194+x, 106+y, 197+x, 112+y); rectangulo_lleno(194+x, 124+y, 236+x, 127+y);
+    rectangulo_lleno(197+x, 103+y, 200+x, 106+y); rectangulo_lleno(197+x, 112+y, 200+x, 115+y);
+    rectangulo_lleno(200+x, 94+y, 203+x, 97+y); rectangulo_lleno(200+x, 100+y, 203+x, 103+y); rectangulo_lleno(200+x, 115+y, 230+x, 118+y);
+    rectangulo_lleno(203+x, 97+y, 209+x, 100+y);
+    rectangulo_lleno(204+x, 127+y, 207+x, 136+y);
+    rectangulo_lleno(209+x, 94+y, 221+x, 97+y);
+    rectangulo_lleno(221+x, 97+y, 227+x, 100+y);
+    rectangulo_lleno(224+x, 127+y, 227+x, 136+y);
+    rectangulo_lleno(227+x, 94+y, 230+x, 97+y); rectangulo_lleno(227+x, 100+y, 230+x, 103+y);
+    rectangulo_lleno(230+x, 103+y, 233+x, 106+y); rectangulo_lleno(230+x, 112+y, 233+x, 115+y);
+    rectangulo_lleno(233+x, 106+y, 236+x, 112+y); rectangulo_lleno(236+x, 121+y, 239+x, 124+y); rectangulo_lleno(236+x, 127+y, 239+x, 130+y);
+    rectangulo_lleno(197+x, 103+y, 200+x, 106+y); rectangulo_lleno(197+x, 112+y, 200+x, 115+y);
+    rectangulo_lleno(200+x, 94+y, 203+x, 97+y); rectangulo_lleno(200+x, 100+y, 203+x, 103+y); rectangulo_lleno(200+x, 115+y, 230+x, 118+y);
+    rectangulo_lleno(203+x, 112+y, 206+x, 115+y);
+    rectangulo_lleno(224+x, 112+y, 227+x, 115+y);
+    rectangulo_lleno(236+x, 112+y, 239+x, 115+y);
+    rectangulo_lleno(239+x, 115+y, 242+x, 121+y);
+    rectangulo_lleno(242+x, 121+y, 245+x, 124+y);
+
+    color_rgb(105, 139, 184); // GRIS AZUL
+    rectangulo_lleno(206+x, 100+y, 209+x, 109+y);
+    rectangulo_lleno(209+x, 103+y, 221+x, 106+y);
+    rectangulo_lleno(212+x, 106+y, 217+x, 109+y);
+
+    color_rgb(80, 77, 120); // MORADO
+    rectangulo_lleno(206+x, 109+y, 224+x, 112+y);
+    rectangulo_lleno(209+x, 112+y, 221+x, 115+y);
+    rectangulo_lleno(221+x, 100+y, 224+x, 112+y);
+
+    color_rgb(225, 225, 225); // BLANCO
+    rectangulo_lleno(194+x, 115+y, 197+x, 118+y);
+    rectangulo_lleno(197+x, 118+y, 203+x, 121+y);
+    rectangulo_lleno(206+x, 118+y, 209+x, 121+y);
+}
+
+
+
+
 
     int jugador1(int x, int y){
 
@@ -123,9 +233,17 @@ using namespace miniwin;
         vredimensiona(500,500);
 
         // Coordenadas del jugador1
-        int jugador1X = 200, jugador1Y = 200;
+        int jugador1X = 200, jugador1Y = 300;
 
+        // Coordenadas del enemigo1
+        int enemigo1X = 50, enemigo1Y = 20;
+
+        // Coordenadas del enemigo2
+        int enemigo2X = 50, enemigo2Y = 20;
+
+        // Valor ingresado al presionar el teclado
         int teclaIngresada = tecla();
+
 
         // Movimiento en el eje X del jugador1
         while(teclaIngresada != ESCAPE){
@@ -141,11 +259,59 @@ using namespace miniwin;
             }
 
 
+
+
+            // Movimiento de la nave enemiga1
+            int aleatorio, direccion;
+
+            aleatorio = rand()%50;
+            if(aleatorio == 0){
+                direccion = IZQUIERDA;
+            }else if(aleatorio == 1){
+                direccion = DERECHA;
+            }
+
+            if(direccion == IZQUIERDA){
+                if( enemigo1X > 0){
+                    enemigo1X -= 15;
+                }
+            }else if (direccion == DERECHA){
+                if (enemigo1X < 350){
+                    enemigo1X += 15;
+                }
+            }
+
+
+            // Movimiento de la nave enemiga2
+            int aleatorio2, direccion2;
+
+            aleatorio2 = rand()%50;
+            if(aleatorio2 == 0){
+                direccion2 = IZQUIERDA;
+            }else if(aleatorio2 == 1){
+                direccion2 = DERECHA;
+            }
+
+            if(direccion2 == IZQUIERDA){
+                if( enemigo2X > -150){
+                    enemigo2X -= 15;
+                }
+            }else if (direccion2 == DERECHA){
+                if (enemigo2X < 225){
+                    enemigo2X += 15;
+                }
+            }
+
+
             borra();
             jugador1(jugador1X, jugador1Y);
+            enemigo1(enemigo1X, enemigo1Y);
+            enemigo2(enemigo2X, enemigo2Y);
             refresca();
             espera(10);
             teclaIngresada = tecla();
+
+
         }
 
 
